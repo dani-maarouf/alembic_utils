35daaff5076a31da75c59fcf9f25586a08396e64
Dani Maarouf <dmaarouf@cisco.com>
2024-05-14 00:31:03 -0400

    Changes for sqlalchemy2

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         "alembic>=1.9",
         "flupy",
         "parse>=1.8.4",
-        "sqlalchemy>=1.3",
+        "sqlalchemy>=2.0",
         "psycopg2-binary>=2.9.3",
         "typing_extensions",
     ],
